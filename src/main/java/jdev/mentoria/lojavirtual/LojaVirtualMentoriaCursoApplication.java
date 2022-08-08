57c92047ac74636f118cf7b32ead808e5e968ad6
William Conan Mateus <williamcsouza2022@gmail.com>
2022-08-08 19:39:46 -0300

    commit inicial projeto Mentoria curso

---
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaCursoApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaCursoApplication.class, args);
+	}
+
+}
