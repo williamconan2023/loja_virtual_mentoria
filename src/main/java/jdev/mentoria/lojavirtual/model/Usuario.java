a29c152479a2cd8f81ee28c27a153fd01af033ba
William Conan Mateus <williamcsouza2022@gmail.com>
2023-06-12 21:07:07 -0300

    Adicionando tipo de endereco e usuario

--- src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
@@ -1,5 +1,86 @@
 package jdev.mentoria.lojavirtual.model;
 
-public class Usuario {
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name="usuario")
+@SequenceGenerator(name="seq_usuario" , sequenceName = "seq_usuario",allocationSize = 1,initialValue = 1)
+public class Usuario implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "seq_usuario")
+	private Long id;
+	
+	private String login;
+	
+	private String senha;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataAtualSenha;
+	
+	private List<Acesso> acessos;
+	
+	
+	
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return this.acessos;
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+	
 
 }
